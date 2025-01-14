3fd964856debb80b269c01a5156ca0bc4ceb0b49
eduardodecarvalho <decarvalho.dev@gmail.com>
2025-01-14 12:52:11 +0100

    add sudoku_leetcode.py

---
+++ sudoku_leetcode.py
@@ -0,0 +1,61 @@
+'''
+Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+
+Each row must contain the digits 1-9 without repetition.
+Each column must contain the digits 1-9 without repetition.
+Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+Note:
+
+A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+Only the filled cells need to be validated according to the mentioned rules.
+'''
+
+
+from typing import DefaultDict, List
+
+
+def isValidSudoku(board: List[List[str]]) -> bool:
+    rows = DefaultDict(set)
+    cols = DefaultDict(set)
+    boxes = DefaultDict(set)
+    
+    for r in range(9):
+        for c in range(9):
+            current = board[r][c]
+            if current == ".":
+                continue
+            if current in rows[r] or current in cols[c] or current in boxes[(r//3), c//3]:
+                return False
+
+            rows[r].add(current)
+            cols[c].add(current)
+            boxes[(r//3, c//3)].add(current)
+
+    return True
+
+
+board_should_return_true = [["5","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+
+print(isValidSudoku(board_should_return_true))
+
+
+board_should_return_false = [["8","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+
+print(isValidSudoku(board_should_return_false))
+
